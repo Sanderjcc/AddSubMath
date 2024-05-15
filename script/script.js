da1cfef896cccddc0fd3c9a034ad1cdf2b388867
Elex Sander <ElexSander.py@gmail.com>
2024-05-15 19:54:03 -0300

    Square Root - grouping numbers
    finishing grouping system

--- script/script.js
+++ script/script.js
@@ -7,16 +7,28 @@ function send(){
     if (squareRoot.length == 0){
         alert('ERROR! Empty Field')
     } else {
-        let cont = 0
+        let cont = 1
         let group = ''
         let groupNumbers = []
         
-        for(let i = Number(squareRoot.length-1); i >=0; i--){
+        for(let i = String(squareRoot.length-1); i >=0; i--){
             if (cont == 2){
+                group = squareRoot[i] + squareRoot[i+1]
+                console.log(`before the add: ${groupNumbers}`)
+                groupNumbers.unshift(group) 
+                console.log(`after the add: ${groupNumbers}`)
+                cont = 1
+            
+            } else if (i == 0){
+                groupNumbers.unshift(squareRoot[i])
                 
+            } else {
+                cont++
             }
+            
+            console.log(i)
         }
-        
+        console.log(groupNumbers)
     }
 
 }
